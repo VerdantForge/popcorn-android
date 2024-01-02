f5e33b7d3a8a7afcdbad35cecac0ae211e23000b
Nicholas Greensmith <123564396+VerdantForge@users.noreply.github.com>
2024-01-02 11:38:40 +0000

    fix paths

--- .devcontainer/install-sdkmanager.sh
+++ .devcontainer/install-sdkmanager.sh
@@ -9,6 +9,9 @@ wget https://dl.google.com/android/repository/$output_file
 unzip $output_file -d $unzip_dir
 sudo mkdir --parents "$ANDROID_HOME/cmdline-tools/latest"
 sudo mv -f $unzip_dir/* "$ANDROID_HOME/cmdline-tools/latest/"
+
+# Cleanup
 rm $output_file
+rm -rf $unzip_dir
 
 export PATH=$ANDROID_HOME/cmdline-tools/latest/bin:$PATH
